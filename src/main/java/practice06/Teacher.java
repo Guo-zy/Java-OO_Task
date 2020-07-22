fbb319fea1915ae28e7b97047fe740571b3c4a45
Guo-zy <674402145@qq.com>
2020-07-22 22:40:29 +0800

    [Yancy] feature:finish practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,28 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+
+    protected int klass;
+
+    public Teacher(String name, int age,int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public Teacher(String name, int age) {
+        super(name, age);
+
+    }
+
+
+    @Override
+    public String introduce() {
+        if(this.getKlass() != 0)
+        return super.introduce() + "I am a Teacher. I teach Class +"+this.getKlass()+".";
+        else
+        return super.introduce() + " I am a Teacher. I teach No Class.";
+    }
+
+    public int getKlass() {
+        return klass;
+    }
 }
