b4ccdda23ad5d12e978f0830ac3ee0bcbb54e0be
Guo-zy <674402145@qq.com>
2020-07-22 22:52:19 +0800

    [Yancy] feature:finish practice07

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,20 @@
 package practice07;
 
-public class Student {
+public class Student extends Person{
+
+    protected  Klass klass;
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + "I am a Student. I am at Class "+this.klass.getNumber()+".";
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
 }
