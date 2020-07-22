03f1ef348877d3a6d1a4f2f39fe5cbff1ad6a163
Guo-zy <674402145@qq.com>
2020-07-22 22:15:14 +0800

    first commit

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,5 @@
 package practice01;
 
 public class Person {
+
 }
--- src/test/java/practice01/Practice01Test.java
+++ src/test/java/practice01/Practice01Test.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
+
+
 public class Practice01Test {
     @Test
     public void should_person_have_name_and_age() throws Exception {
