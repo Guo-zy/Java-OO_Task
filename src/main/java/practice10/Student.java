2b9cf8d6249fd7dfdeed36d2c96f2ecfe950c4b1
Guo-zy <674402145@qq.com>
2020-07-22 23:44:02 +0800

    [Yancy] feature:finish practice10

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,25 @@
 package practice10;
 
-public class Student {
+
+
+public class Student extends Person{
+    protected Klass klass;
+
+    public Student(int id , String name, int age, Klass klass) {
+        super(id,name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        Student leader = this.getKlass().getLeader();
+        if(leader != null && leader.getName().equals(this.getName()))
+            return super.introduce() + "I am a Student. I am Leader of Class "+this.klass.getNumber()+".";
+        else
+            return super.introduce() + "I am a Student. I am at Class "+this.klass.getNumber()+".";
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
 }
