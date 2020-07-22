b9923db92d85fa95ffb228ee19bcdd4155391d1d
Guo-zy <674402145@qq.com>
2020-07-22 23:10:45 +0800

    [Yancy] feature:finish practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,28 @@
 package practice08;
 
 public class Klass {
+    protected int number;
+    protected Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName(){
+        return "Class "+this.getNumber();
+    }
+
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student student){
+        this.leader = student;
+    }
 }
+
--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,27 @@
 package practice08;
 
 public class Person {
+    protected  int id;
+    protected String name;
+    protected  int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce(){
+        return "My name is +"+this.name+". I am "+this.age+" years old.";
+    }
+
+
 }
