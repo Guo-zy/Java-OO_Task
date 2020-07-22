c291de72ac2e0ebc005d3753f36f52ec833ad388
Guo-zy <674402145@qq.com>
2020-07-22 22:23:32 +0800

    [Yancy] feature:finish practice01

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -11,6 +11,10 @@ public class Person {
     public int getAge() {
         return age;
     }
+    public Person(String name , int age){
+        this.name = name;
+        this.age = age;
+    }
 
     public String introduce(){
         return "My name is +"+this.name+". I am "+this.age+" years old.";
